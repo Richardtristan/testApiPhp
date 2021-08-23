bb00626f0832ad97bc79ae23e6dcd2b0a5fcb07a
richard <richard.tristan.93@gmail.com>
2021-08-23 22:50:22 +0200

    first commit

--- index.php
+++ index.php
@@ -2,24 +2,22 @@
 require "vendor/autoload.php";
 $photo = array();
 for ($number = 0; $number < 10; $number++) {
-    $minmissions = file_get_contents("https://api.magicthegathering.io/v1/cards?page= $number");
+    $minmissions = file_get_contents("https://api.magicthegathering.io/v1/cards?language=french&page=$number&colors=Green");
     $responses = json_decode($minmissions, true);
     $key = $responses['cards'];
     foreach ($key as $value) {
         if (array_key_exists('imageUrl', $value)) {
-            array_push($photo,"{$value['imageUrl']},{$value['multiverseid']}");
+            array_push($photo,array('url'=>$value['imageUrl'],'colors'=>$value['colors'],'id'=>$value['multiverseid']));
         }
     }
 }
 
 /*echo "<img alt='' src=" . $value['imageUrl'] . "><br>";*/
-
-
-
+rsort($photo);
 foreach ($photo as $value){
-    echo $value . '<br>';
+    echo "<img alt='' src='  {$value['url'] } ' >";
+    echo $value['id'];
 }
-
 
 
 
